dd57d0f72771047b7e29de85a890e41ba9680ebf
Jitendra singh <jitendrasingh6013815@gmail.com>
2024-07-11 02:49:43 +0530

    Added link to the social handles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,29 @@
 import logo from "../assets/jitendraSinghLogo2.png";
-import { FaLinkedin } from "react-icons/fa6";
-import { FaGithub } from "react-icons/fa6";
-import { FaSquareXTwitter } from "react-icons/fa6";
-import { FaInstagram } from "react-icons/fa6";
+import { FaLinkedin, FaGithub, FaSquareXTwitter, FaInstagram } from "react-icons/fa6";
 
 const Navbar = () => {
-  return (<nav className="mb-20 flex items-center justify-between py-6">
-    <div className="flex flex-shrink-0 items-center">
-        <img className="mx-2 w-20" src={logo} alt="logo"/>
-    </div>
-    <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <FaLinkedin />
-        <FaGithub />
-        <FaSquareXTwitter />
-        <FaInstagram />
-    </div>
-  </nav>
+  return (
+    <nav className="mb-20 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <img className="mx-2 w-20" src={logo} alt="logo" />
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <a href="https://www.linkedin.com/in/jitendra-singh-179089284" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin />
+        </a>
+        <a href="https://github.com/Jitenndra03" target="_blank" rel="noopener noreferrer">
+          <FaGithub />
+        </a>
+        <a href="https://twitter.com/Jitendra_14_09" target="_blank" rel="noopener noreferrer">
+          <FaSquareXTwitter />
+        </a>
+        <a href="https://www.instagram.com/jitendra_14_09/?hl=en" target="_blank" rel="noopener noreferrer">
+          <FaInstagram />
+        </a>
+      </div>
+    </nav>
   );
 };
 
 export default Navbar;
+
